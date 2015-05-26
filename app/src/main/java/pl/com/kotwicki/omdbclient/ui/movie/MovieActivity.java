8b6214b4ba0b7af2e502218bb9567eddc7dbc4e4
Filip Kotwicki <f.kotwicki@ambsoft.pl>
2015-05-26 15:23:32 +0200

    small fix

--- app/src/main/java/pl/com/kotwicki/omdbclient/ui/movie/MovieActivity.java
+++ app/src/main/java/pl/com/kotwicki/omdbclient/ui/movie/MovieActivity.java
@@ -29,6 +29,7 @@ public class MovieActivity extends BaseActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             finish();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
